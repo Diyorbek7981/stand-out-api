d796717f8f899312beea70991f0f64ed6e86d33e
Diyorbek7981 <abdurahimov.diyorbek7981@gmail.com>
2025-10-22 19:28:31 +0500

    stapi285

--- drfapp/serializers.py
+++ drfapp/serializers.py
@@ -6,5 +6,5 @@ class UsersSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = (
-            "id", "telegram_id", "first_name", "user_name", "age", "certificate", "phone_number", "is_channel_member",
+            "id", "telegram_id", "first_name", "user_name", "age", "certificate", "phone_number",
             "is_confirmed", "is_registered", "language")
